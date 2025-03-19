c197bc13082fa542a70c77847d52d379722a920e
Gabriel Moraes <gabrieltrabalho96@hotmail.com>
2025-03-18 22:48:04 -0300

    Feito a tela de acesso, codigo atrás da mesma

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,46 @@
+import React, { use } from 'react'
+import { useState } from 'react'
+import { FaUser, FaLock } from 'react-icons/fa'
+import "./Login.css"
+
+
+
+const Login = () => {
+     const [username, setUsername] = useState("")
+     const [password, setPassword] = useState("")
+
+     const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("envio")
+     }
+
+  return (
+    <div className='container'>
+        <form onSubmit={handleSubmit}>
+           <h1>Acesse o sistema</h1>
+           <div className='input-field'>
+            <input type="text" placeholder='Digite o login' onChange={ (e) =>setUsername (e.target.value)}/>
+            <FaUser className="icon"/>
+           </div>
+           <div className='input-field'>
+            <input type="text" placeholder='Digite a senha' onChange={ (e) =>setPassword (e.target.value)}/>
+            <FaLock className='icon'/>
+           </div>
+
+            <div className="recall-forget">
+                <label htmlFor="">
+                    <input type="checkbox" />
+                    Lembrar de mim ?
+                </label>
+                <br />
+                <a href="">Esqueceu a senha?</a>
+            </div>
+            <div className=''></div>
+           <button className='btn'>Entrar</button>
+        </form>
+        <p className='account' >Não tem uma conta? <a href="">Registrar</a></p>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/mui/ButtonLogin.jsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import Stack from '@mui/material/Stack';
+// import Button from '@mui/material/Button';
+
+// export default function ColorButtons() {
+//   return (
+//     <Stack direction="row" spacing={2}>
+//       <Button color="secondary">Secondary</Button>
+//       <Button variant="contained" color="success">
+//         Success
+//       </Button>
+//     </Stack>
+//   );
+// }
